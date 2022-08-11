ba481c5244841ce7742c309285355ca372cdb320
Mark Hall <mark.hall993@gmail.com>
2022-08-11 12:20:57 -0400

    Eslint history.

--- history/src/index.js
+++ history/src/index.js
@@ -5,15 +5,15 @@ const amqp = require('amqplib');
 
 // Throw errors if ENV variables are not set.
 if (!process.env.DBHOST) {
-  throw new Error("Please specify the database host using DBHOST.");
+  throw new Error('Please specify the database host using DBHOST.');
 }
 
 if (!process.env.DBNAME) {
-  throw new Error("Please specify the database name using DBNAME.");
+  throw new Error('Please specify the database name using DBNAME.');
 }
 
 if (!process.env.RABBIT) {
-  throw new Error("Please specify the RabbitMQ host using RABBIT.");
+  throw new Error('Please specify the RabbitMQ host using RABBIT.');
 }
 
 // Set ENV Variables.
@@ -24,9 +24,9 @@ const RABBIT = process.env.RABBIT;
 // Connects to MongoDB using ENV names.
 function connectDb() {
   return mongodb.MongoClient.connect(DBHOST)
-    .then(client => {
-      return client.db(DBNAME);
-    });
+      .then((client) => {
+        return client.db(DBNAME);
+      });
 }
 
 // Connects to RabbitMQ using ENV variable.
@@ -35,15 +35,15 @@ function connectRabbit() {
 
   // Connect to the RabbitMQ server.
   return amqp.connect(RABBIT)
-    .then(messagingConnection => {
+      .then((messagingConnection) => {
       // Create a RabbitMQ messaging channel.
-      return messagingConnection.createChannel();
-    });
+        return messagingConnection.createChannel();
+      });
 }
 
 // Set up event handlers.
 function setupHandlers(app, db, messageChannel) {
-  const videoCollection = db.collection("videos");
+  const videoCollection = db.collection('videos');
 
   // Handler for coming messages.
   function consumeViewedMessage(msg) {
@@ -51,28 +51,30 @@ function setupHandlers(app, db, messageChannel) {
     const parsedMsg = JSON.parse(msg.content.toString());
 
     // Record the view in the DB.
-    return videoCollection.insertOne({ videoPath: parsedMsg.videoPath })
-      .then(() => messageChannel.ack(msg)); // If no error, ack message.
+    return videoCollection.insertOne({videoPath: parsedMsg.videoPath})
+        .then(() => messageChannel.ack(msg)); // If no error, ack message.
   };
 
   // Assert that we have a viewed queue.
-  return messageChannel.assertExchange("viewed", "fanout")
-    .then(() => {
+  return messageChannel.assertExchange('viewed', 'fanout')
+      .then(() => {
       // Start receiving messages from the 'viewed' queue.
-      return messageChannel.assertQueue("", { exclusive: true });
-    })
-    .then(response => {
-      const queueName = response.queue;
-      return messageChannel.bindQueue(queueName, "viewed", "")
-        .then(() => messageChannel.consume(queueName, consumeViewedMessage));
-    });
+        return messageChannel.assertQueue('', {exclusive: true});
+      })
+      .then((response) => {
+        const queueName = response.queue;
+        return messageChannel.bindQueue(queueName, 'viewed', '')
+            .then(() => {
+              messageChannel.consume(queueName, consumeViewedMessage);
+            });
+      });
 }
 
 
 // With the DB and RabbitMQ connection, launches an express HTTP Server.
 function startHttpServer(db, messageChannel) {
   // Wrap in promise so we can be notified that the server has started.
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     const app = express();
     // Enable JSON body for HTTP requests.
     app.use(bodyParser.json());
@@ -89,18 +91,18 @@ function startHttpServer(db, messageChannel) {
 // Connect to DB and RabbitMQ, then start Web Server.
 function main() {
   return connectDb()
-    .then(db => {
-      return connectRabbit()
-        .then(messageChannel => {
-          return startHttpServer(db, messageChannel);
-        });
-    });
+      .then((db) => {
+        return connectRabbit()
+            .then((messageChannel) => {
+              return startHttpServer(db, messageChannel);
+            });
+      });
 }
 
 // Execute.
 main()
-  .then(() => console.log("History Microservice Online."))
-  .catch(err => {
-    console.error("History Microservice failed to start.");
-    console.error(err && err.stack || err);
-  });
+    .then(() => console.log('History Microservice Online.'))
+    .catch((err) => {
+      console.error('History Microservice failed to start.');
+      console.error(err && err.stack || err);
+    });
